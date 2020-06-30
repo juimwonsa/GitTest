5229146069deedf074e12fa0800907e967408241
HeeHoon Kim <fnaticisop@gmail.com>
2020-06-30 11:52:32 +0900

    character init

--- src/game/CharacterManager.java
+++
@@ -1,8 +0,0 @@
-package game;
-
-public class CharacterManager {
-	public static void main(String[] args) {
-		Character kim = new Character("kim",1);
-		kim.getItem(new Item("itemName","itemType",1,2));
-	}
-}
